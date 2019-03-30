d2c6dbd35916759ce17b4d4db894ae5024e0af00
kei-mo <keita0215mctt@gmail.com>
2019-03-30 20:58:18 +0900

    update

---
+++ AtCoder/ABC/ABC117/d_2.py
@@ -0,0 +1,30 @@
+#%%
+# nk = list(map(int,input().split()))
+nk = [4,9]
+
+#%%
+N,K = nk[0], nk[1]
+Kbin = bin(K)[2:] # srt
+len_Kbin = len(Kbin)
+
+
+#%%
+# li = list(map(int,input().split()))
+Xs = [7, 4, 0, 3]
+Xs_bin = [int(bin(X)[2:]) for X in Xs if X <=K]# int
+Xs_bin_sum = sum(Xs_bin)
+# Xs_bin_sum
+
+#%%
+ans_bin
+for i in range(len_Kbin):
+    if Kbin[i] == '1'
+        
+# flip
+
+
+#%%
+print(int(flip,2))
+
+
+#%%
